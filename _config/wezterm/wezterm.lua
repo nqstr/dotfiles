c42c9fab1b0553911e4832dd6db98750ed5aeec3
nqstr <oleksandrky@wix.com>
2025-01-10 16:29:47 +0100

    flush

---
+++ _config/wezterm/wezterm.lua
@@ -0,0 +1,48 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.font = wezterm.font 'JetBrainsMono Nerd Font'
+config.scrollback_lines = 10000
+config.window_close_confirmation = 'NeverPrompt'
+
+config.keys = {
+    -- Bind Ctrl+Shift+PageDown to split vertically (create a new pane to the right)
+    {
+        key = "PageDown",
+        mods = "CTRL|SHIFT",
+        action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" },
+    },
+    -- Bind Ctrl+Shift+PageUp to split horizontally (create a new pane below)
+    {
+        key = "PageUp",
+        mods = "CTRL|SHIFT",
+        action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" },
+    },
+}
+
+config.mouse_bindings = {
+    {
+      event = { Up = { streak = 1, button = 'Left' } },
+      mods = 'NONE',
+      action = wezterm.action_callback(function(window, pane)
+        local url = window:get_selection_text_for_pane(pane)
+
+        -- Check if the URL starts with 'file://'
+        if url:match('^file://') then
+          -- Extract the file path from the URL by removing the 'file://' prefix
+          local file_path = url:gsub('^file://', '')
+
+          -- Use VSCode to open the file in the existing window
+          wezterm.log_info("Opening in existing VSCode window: " .. file_path)
+          wezterm.run_child_process({ 'code', '--reuse-window', file_path })
+        end
+      end),
+    },
+}
+
+--wezterm.on('gui-startup', function(cmd)
+--  local tab, pane, window = mux.spawn_window(cmd or {})
+--  window:gui_window():maximize()
+--end)
+
+return config
---
+++ _hammerspoon/init.lua
@@ -0,0 +1,23 @@
+hs.hotkey.bind({ "Alt" }, "z", function()
+     wez = hs.application.find("Wezterm")
+     if wez then
+         if wez:isFrontmost() then
+             wez:hide()
+         else
+             local nowspace = hs.spaces.focusedSpace()
+             local screen = hs.screen.mainScreen()
+             wez_window = wez:mainWindow()
+             hs.spaces.moveWindowToSpace(wez_window, nowspace)
+             local max = screen:fullFrame()
+             local f = wez_window:frame()
+             f.x = max.x
+             f.y = max.y
+             f.w = max.w
+             f.h = max.h * 0.55
+             hs.timer.doAfter(0.2, function()
+                 wez_window:setFrame(f)
+             end)
+             wez_window:focus()
+          end
+     end
+ end)
